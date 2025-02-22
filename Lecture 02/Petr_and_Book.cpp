18edb1ae52aa6797876a387d2ecec359e1928819
Mayank Jha <70832880+mayank7jha1@users.noreply.github.com>
2025-02-22 10:10:06 +0530

    Update Petr_and_Book.cpp

--- Lecture 02/Petr_and_Book.cpp
+++ Lecture 02/Petr_and_Book.cpp
@@ -11,36 +11,33 @@ int main() {
 
 	int sum = m + t + w + th + f + sa + s;
 	n = n % sum;
-
-	if (n == 0) {
-		cout << 7 << endl;
-		return 0;
-	}
-
+  if(n==0){
+    n=sum;
+  }
 
 	while (n >= 0) {
-
-		count = count + 1;
-
-		if (count == 1) {
-			n = n - m;
-		} else if (count == 2) {
-			n = n - t;
-		} else if (count == 3) {
-			n = n - w;
-		} else if (count == 4) {
-			n = n - th;
-		} else if (count == 5) {
-			n = n - f;
-		} else if (count == 6) {
-			n = n - sa;
-		} else {
-			n = n - s;
-		}
+		  n = n - cd;
+			if(n<=0){
+			  cout<<count<<endl;
+			  return 0;
+			}
+			count = count + 1;
+			if (count == 1) {
+				cd = m;
+			} else if (count == 2) {
+				cd = t;
+			} else if (count == 3) {
+				cd = w;
+			} else if (count == 4) {
+				cd = th;
+			} else if (count == 5) {
+				cd = f;
+			} else if (count == 6) {
+				cd = sa;
+			} else {
+				cd = s;
+			}
 	}
-
-
-
 
 	cout << count << endl;
 
